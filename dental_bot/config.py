cf701dbe324156c8c6f7b2aa8ef96888db684759
inikolaev2003-bit <inikolaev2003@gmail.com>
2026-06-01 15:23:18 +0300

    Update BOT_TOKEN to use placeholder value

--- dental_bot/config.py
+++ dental_bot/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8404661720:AAE6FKyux6DlOoysgSVLuv5VZdtqLrUmzj4")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "123456")
 ADMIN_COMMAND = "/admin"
